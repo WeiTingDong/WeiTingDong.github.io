5502f2780acf57dda23aaa90e9b33000abf69544
Weiting <dongwei@dongweideMacBook-Pro.local>
2018-04-10 23:48:22 +0800

    improve gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,15 @@ var gulpIf = require('gulp-if')
 var browserSync = require('browser-sync');
 var useref = require('gulp-useref');
 var uglify = require('gulp-uglify');
+var cleanCSS = require('gulp-clean-css');
+// var minifyCSS = require('gulp-minify-css');
+// var concat = require('gulp-concat')
 
 gulp.task('browserSync', function() {
   browserSync({
-    server: '.'
+    server: {
+      baseDir:'app'
+    }
   })
 })
 
@@ -18,10 +23,24 @@ gulp.task('watch', ['browserSync'], function (){
 });
 
 gulp.task('useref', function(){
-  return gulp.src('*.html')
+  return gulp.src('app/*.html')
     .pipe(useref())
+    .pipe(gulpIf('*.css',cleanCSS())) // Minifies CSS files
     .pipe(gulpIf('*.js',uglify())) // Uglifies Javascript files
-    .pipe(gulp.dest('dist'))
+    .pipe(gulp.dest('.'))
+    // .pipe(gulpIf('!*.html',gulp.dest('dist')))
+    // .pipe(gulpIf('*.html',gulp.dest('.')))
 });
 
-gulp.task('default', ['watch','browserSync','useref']);
+gulp.task('copy-imgs',function(){
+  return gulp.src('app/imgs/*.JPG')
+  .pipe(gulp.dest('imgs'))
+})
+
+// gulp.task('styles',function(){
+//   return gulp.src('*.html')
+//     .pipe(concat('styles.min.css'))
+//     .pipe(gulp.dest('dist'))
+// })
+
+gulp.task('default', ['watch','browserSync','useref','copy-imgs']);
