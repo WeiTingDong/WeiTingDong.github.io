d3bfe923c16e9e169193b5d55bb9b91853b3b794
Weiting Dong <Weiting@weiting.cn.ibm.com>
2018-04-10 17:43:12 +0800

    uglify and useref

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,12 @@
 var gulp = require('gulp');
+var gulpIf = require('gulp-if')
 var browserSync = require('browser-sync');
 var useref = require('gulp-useref');
 var uglify = require('gulp-uglify');
 
 gulp.task('browserSync', function() {
   browserSync({
-    server: {
-      baseDir: 'app'
-    },
+    server: '.'
   })
 })
 
@@ -15,19 +14,14 @@ gulp.task('watch', ['browserSync'], function (){
   gulp.watch('app/css/*.css', browserSync.reload);
   // Reloads the browser whenever HTML or JS files change
   gulp.watch('app/*.html', browserSync.reload);
-  gulp.watch('app/js/*.js', browserSync.reload);
+  gulp.watch('app/js/*.js', ['uglify',browserSync.reload]);
 });
 
-// gulp.task('useref', function(){
-//   return gulp.src('app/*.html')
-//     .pipe(uglify()) // Uglifies Javascript files
-//     .pipe(useref())
-//     .pipe(gulp.dest('dist'))
-// });
-
-gulp.task('default', function() {
-    // gulp.run('browserSync');
-    gulp.run('watch');
-    // gulp.run('useref');
-
+gulp.task('useref', function(){
+  return gulp.src('*.html')
+    .pipe(useref())
+    .pipe(gulpIf('*.js',uglify())) // Uglifies Javascript files
+    .pipe(gulp.dest('dist'))
 });
+
+gulp.task('default', ['watch','browserSync','useref']);
